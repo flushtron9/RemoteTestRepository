312044394d8167e2509ee6f996bce4d1ab48d336
Suresh Kada <suresh.kada@gmail.com>
2016-04-20 11:56:40 -0400

    Completed formatting message

--- GitTestProject/src/com/suresh/git/test/GitTest.java
+++ GitTestProject/src/com/suresh/git/test/GitTest.java
@@ -3,9 +3,9 @@ package com.suresh.git.test;
 public class GitTest {
 
 	public void showMessage(){
-		System.out.println("@@1st Message shown##");
-		System.out.println("@@2nd Message shown##");
-		System.out.println("@@2nd Message shown##");
+		System.out.println("1111@@1st Message shown##");
+		System.out.println("2222@@2nd Message shown##");
+		System.out.println("3333@@2nd Message shown##");
 		
 	}
 }
