2fa29634bfe6700610065eb5cfb0557b5817ef40
Suresh Kada <suresh.kada@gmail.com>
2016-04-20 08:37:24 -0400

    New change for message

--- GitTestProject/src/com/suresh/git/test/GitTest.java
+++ GitTestProject/src/com/suresh/git/test/GitTest.java
@@ -3,6 +3,7 @@ package com.suresh.git.test;
 public class GitTest {
 
 	public void showMessage(){
-		System.out.println("Message shown");
+		System.out.println("1st Message shown");
+		System.out.println("2nd Message shown");
 	}
 }
