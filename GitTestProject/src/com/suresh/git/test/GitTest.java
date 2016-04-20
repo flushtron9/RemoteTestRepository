937b07cf90265ed356826bddbd47c72cb645e404
Suresh Kada <suresh.kada@gmail.com>
2016-04-20 11:32:32 -0400

    Added message 3

--- GitTestProject/src/com/suresh/git/test/GitTest.java
+++ GitTestProject/src/com/suresh/git/test/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
 	public void showMessage(){
 		System.out.println("1st Message shown");
 		System.out.println("2nd Message shown");
+		System.out.println("2nd Message shown");
 	}
 }
